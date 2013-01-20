0518a20ff96966d20059c645347e1adc33cf3274
David Rhoderick <broken.stairs@gmail.com>
2013-01-20 12:23:31 -0500

    Declaring functions and variables in testApp.h and initializing some definitions for variables in testApp.cpp

--- of/GoI_Control_v1_0/src/testApp.h
+++ of/GoI_Control_v1_0/src/testApp.h
@@ -19,4 +19,19 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		// readCSV reads the data from the CSV file and assigns it to the variables
+		void readCSV();
+		// convertAngle changes the angle of the sunlight into the values for the light array
+		void convertAngle(int angle, string blueOrRed);
+		// sendToArduino sends commands to the Arduino for controlling the grow box
+		void sendToArduino();
+		// receiveFromArduino receives data from the Arduino
+		void receiveFromArduino();
+		// saves data from the grow box to a file
+		void saveToFile();
+		
+		Array lightArrayBlue[];	// lightArrayBlue contains the 5 lights' values for the blue light array
+		Array lightArrayRed[];	// lightArrayRed contains  the 5 lights' values for the red light array
+		float temperature;		// temperature is the target temperature for the grow box
+		bool water;				// water is TRUE when a water command needs to be issued and FALSE otherwise
 };
